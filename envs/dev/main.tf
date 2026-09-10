95441b82cd5f03caeab08aa1fff4584470289909
kanu <kanu.paul@gmail.com>
2026-09-10 12:05:57 +0930

    dev: TF WF  at:  Thursday, September 10, 2026 at 12:05 PM

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -27,7 +27,7 @@ module "eks" {
   subnet_ids         = module.vpc.private_subnets
   kubernetes_version = "1.35"
   instance_types     = ["t3.medium"]
-  min_size           = 2
+  min_size           = 1
   max_size           = 3
   desired_size       = 2
 }
